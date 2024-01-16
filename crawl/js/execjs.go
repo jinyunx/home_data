d5c3bac122f4798e29818d5451886033bd3b39ef
onexie <onexie@tencent.com>
2024-01-16 23:04:50 +0800

    fix

--- crawl/js/execjs.go
+++ crawl/js/execjs.go
@@ -4,13 +4,22 @@ import (
 	b64 "encoding/base64"
 	"log"
 	"os/exec"
+	"path/filepath"
+	"runtime"
 )
 
 func DecryptImage(input []byte) ([]byte, error) {
 	sEnc := b64.StdEncoding.EncodeToString(input)
 
+	// 获取当前文件的路径
+	_, filename, _, _ := runtime.Caller(0)
+
+	// 获取当前文件的目录
+	dir := filepath.Dir(filename)
+	jsFile := filepath.Join(dir, "decrypt.js")
+
 	// 调用 Node.js 执行 JavaScript 文件
-	cmd := exec.Command("node", "decrypt.js", sEnc)
+	cmd := exec.Command("node", jsFile, sEnc)
 	output, err := cmd.Output()
 	if err != nil {
 		log.Println("Error:", err)
