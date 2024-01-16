c4a42bb27e0a04454a27779e7bccec496526799c
onexie <onexie@tencent.com>
2024-01-17 00:02:58 +0800

    add MergeImg

--- crawl/image.go
+++ crawl/image.go
@@ -1,9 +1,13 @@
 package crawl
 
 import (
+	"bytes"
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
+	"github.com/disintegration/imaging"
 	"github.com/jinyunx/home_data/crawl/js"
+	"image"
+	"image/jpeg"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -69,6 +73,98 @@ func SaveImg(detailUrl string) error {
 	}
 	for i, img := range imgs {
 		os.WriteFile(strconv.Itoa(i)+".jpg", img, os.ModePerm)
+	}
+	MergeImg(imgs)
+	return nil
+}
+
+func MergeImg(imgs [][]byte) error {
+	var imgDecoded []image.Image
+	height := 0
+	width := 800
+	for _, img := range imgs {
+		item, _, err := image.Decode(bytes.NewReader(img))
+		if err != nil {
+			log.Println()
+			return err
+		}
+		height += item.Bounds().Dy()
+		imgDecoded = append(imgDecoded, item)
+	}
+
+	// 长图
+	if true {
+		longImg := imaging.New(width, height, image.Transparent)
+		y := 0
+		for _, img := range imgDecoded {
+			if img.Bounds().Dx() != width {
+				img = imaging.Resize(img, width, 0, imaging.Lanczos)
+			}
+
+			longImg = imaging.Paste(longImg, img, image.Pt(0, y))
+			y += img.Bounds().Dy()
+		}
+		// 创建新的图片文件
+		longFile, err := os.Create("longImg.jpg")
+		if err != nil {
+			log.Println("os.Create fail", err)
+			return err
+		}
+		defer longFile.Close()
+
+		// 将拼接后的图片编码为JPEG格式并保存到新的文件
+		jpeg.Encode(longFile, longImg, &jpeg.Options{Quality: jpeg.DefaultQuality})
+
+	}
+
+	if true {
+		imgCnt := len(imgDecoded)
+		if imgCnt > 8 {
+			imgCnt = 8
+		}
+
+		numLine := 2
+		imgCntOneLine := imgCnt / 2
+		if imgCnt < 4 {
+			imgCntOneLine = imgCnt
+			numLine = 1
+		}
+
+		splitHeight1 := 0
+		splitHeight2 := 0
+		var imgResize []image.Image
+		for i, img := range imgDecoded {
+			if i > numLine*imgCntOneLine {
+				break
+			}
+			img = imaging.Resize(img, width/imgCntOneLine, 0, imaging.Lanczos)
+			imgResize = append(imgResize, img)
+
+			if img.Bounds().Dy() > splitHeight1 && len(imgResize) <= imgCntOneLine {
+				splitHeight1 = img.Bounds().Dy()
+			}
+			if img.Bounds().Dy() > splitHeight2 && len(imgResize) > imgCntOneLine {
+				splitHeight2 = img.Bounds().Dy()
+			}
+		}
+		shortImg := imaging.New(width, splitHeight1+splitHeight1, image.Transparent)
+		for i, img := range imgResize {
+			x := (i % imgCntOneLine) * (width / imgCntOneLine)
+			y := (i / imgCntOneLine) * splitHeight1
+			shortImg = imaging.Paste(shortImg, img, image.Pt(x, y))
+		}
+
+		// 创建新的图片文件
+		shortFile, err := os.Create("short.jpg")
+		if err != nil {
+			log.Println("os.Create fail", err)
+			return err
+		}
+		defer shortFile.Close()
+
+		// 将拼接后的图片编码为JPEG格式并保存到新的文件
+		jpeg.Encode(shortFile, shortImg, &jpeg.Options{Quality: jpeg.DefaultQuality})
+
 	}
 	return nil
 }
